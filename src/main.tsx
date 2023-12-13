ef54385aac0e6e1544f2477f50896312021525d7
choi joo young <68840733+cwd3469@users.noreply.github.com>
2023-12-13 22:54:03 +0900

    OVB-1 chore : react-router-dom 추가 적용

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
+
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import MainPage from '@/pages/MainPage';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <MainPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
