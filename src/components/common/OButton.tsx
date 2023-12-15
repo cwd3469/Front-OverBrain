cd0330bfa2b21d3f78dbc9856d15e990ab2cb73b
choi joo young <68840733+cwd3469@users.noreply.github.com>
2023-12-15 16:50:01 +0900

    OVB-1 feat : OButton 초기 개발 시작

---
+++ src/components/common/OButton.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const OButton = (props: { children: JSX.Element | JSX.Element[] | string | number }) => {
+  const { children } = props;
+  return (
+    <button
+      type="submit"
+      className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+    >
+      {children}
+    </button>
+  );
+};
+
+export default OButton;
