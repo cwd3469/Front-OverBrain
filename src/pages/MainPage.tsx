6d078505d035dfa66a87ae96b3a128f9aab5f704
choi joo young <68840733+cwd3469@users.noreply.github.com>
2023-12-21 00:36:45 +0900

    OVB-1 feat : MainPage todo 기본 적용

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,14 +1,38 @@
-import OButton from '@components/common/OButton';
-import OLayout from '@components/common/OLayout';
+import React, { useState } from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { OLayout, OTextField, OCard, OButton } from '@components/common/index';
+
+type Inputs = {
+  todo: string;
+};
 
 const MainPage = () => {
+  const [todo, setTodo] = useState<string[]>([]);
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>();
+
+  const onSubmit: SubmitHandler<Inputs> = (data) => {
+    setTodo((prev) => {
+      return [...prev, data.todo];
+    });
+  };
   return (
     <OLayout>
-      <div className="flex flex-row items-center gap-3">
-        <div className="bg-primary-400"></div>
-        <OButton size="large">label</OButton>
-        <OButton size="medium">label</OButton>
-        <OButton size="small">label</OButton>
+      <div className="flex flex-col gap-2">
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <OTextField {...register('todo', { required: true })} />
+          <OButton type="submit" size="small">
+            저장
+          </OButton>
+        </form>
+        {errors.todo && <span>This field is required</span>}
+        {todo.map((item, index) => {
+          return <OCard key={index}>{item}</OCard>;
+        })}
       </div>
     </OLayout>
   );
