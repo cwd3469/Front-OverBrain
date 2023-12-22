5bdfef28e1aca3118f3a2826ff4603cbbf120a10
choi joo young <68840733+cwd3469@users.noreply.github.com>
2023-12-22 11:01:35 +0900

    OVB-1 feat : SigninPage 초기 개발

--- src/main.tsx
+++ src/main.tsx
@@ -3,11 +3,16 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainPage from '@/pages/MainPage';
+import SigninPage from '@/pages/SigninPage';
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <MainPage />,
+  },
+  {
+    path: '/signin',
+    element: <SigninPage />,
   },
 ]);
 
