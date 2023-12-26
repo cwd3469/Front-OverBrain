232343ecf3068db5a6a60c7751b33219689843f0
choi joo young <68840733+cwd3469@users.noreply.github.com>
2023-12-26 18:09:52 +0900

    OVB-1 feat : OLayout container 적용

--- src/components/common/OLayout.tsx
+++ src/components/common/OLayout.tsx
@@ -12,7 +12,7 @@ const OLayout = ({ children }: Props) => {
   return (
     <div className="h-screen">
       <div className="flex flex-col justify-center flex-1 min-h-full px-6 py-12 lg:px-8 ">
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">{children}</div>
+        <div className="container p-3 bg-slate-200">{children}</div>
       </div>
     </div>
   );
