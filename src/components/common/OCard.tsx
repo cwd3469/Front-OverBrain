5cd1b223e2a755c4237ede8f6134e6de1034335b
choi joo young <68840733+cwd3469@users.noreply.github.com>
2023-12-27 16:44:29 +0900

    OVB-1 feat : OCard 수정

--- src/components/common/OCard.tsx
+++ src/components/common/OCard.tsx
@@ -2,7 +2,7 @@ type Props = {
   children?: React.ReactNode;
 };
 const OCard = ({ children }: Props) => {
-  return <div className="flex flex-col bg-TG000 rounded-xl	shadow-lg p-4">{children}</div>;
+  return <div className="flex flex-col p-4 bg-white shadow-lg bg-TG000 rounded-xl">{children}</div>;
 };
 
 export default OCard;
