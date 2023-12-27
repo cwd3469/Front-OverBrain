e9a28760961d43f086ffbef41474cb333e49effb
choi joo young <68840733+cwd3469@users.noreply.github.com>
2023-12-27 18:13:47 +0900

    OVB-1 feat : OTextarea 높이 조절

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -32,7 +32,16 @@ const MainPage = () => {
         <div className="flex flex-col w-full gap-2 sm:w-1/2 lg:w-1/3 xl:w-144">
           <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
             <OTextField {...register('title')} placeholder="목표를 작성해 주세요." />
-            <OTextarea {...register('contents')} placeholder="구체적으로 작성해 주세요." />
+            <OTextarea
+              {...register('contents')}
+              placeholder="구체적으로 작성해 주세요."
+              style={{
+                width: '100%',
+                height: '20.25em',
+                border: 'none',
+                resize: 'none',
+              }}
+            />
             <OTextField {...register('startAt')} type="date" label="시작 날짜" />
             <OTextField {...register('endAt')} type="date" label="끝 날짜" />
             <OButton type="submit" size="small">
