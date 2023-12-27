40d2a3c6f3e1aaf0943466960ee4b4866bbe354d
choi joo young <68840733+cwd3469@users.noreply.github.com>
2023-12-27 17:51:03 +0900

    OVB-1 feat : BoardPage router 설정

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import MainPage from '@/pages/MainPage';
 import SigninPage from '@/pages/SigninPage';
+import BoardPage from '@/pages/BoardPage';
 
 const router = createBrowserRouter([
   {
@@ -13,6 +14,10 @@ const router = createBrowserRouter([
   {
     path: '/signin',
     element: <SigninPage />,
+  },
+  {
+    path: '/board',
+    element: <BoardPage />,
   },
 ]);
 
