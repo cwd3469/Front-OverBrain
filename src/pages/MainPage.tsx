718720ef916870ed9e6db9f88e85f3934a40c5ec
choi joo young <68840733+cwd3469@users.noreply.github.com>
2023-12-27 17:51:51 +0900

    OVB-1 feat : MainPage 디자인 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { OLayout, OTextField, OCard, OButton } from '@components/common/index';
+import { OLayout, OTextField, OButton } from '@components/common/index';
 import { useNavigate } from 'react-router-dom';
 import { Target } from '@/types/main';
 import OTextarea from '@components/common/OTextarea';
@@ -18,31 +18,18 @@ const MainPage = () => {
   const onSubmit: SubmitHandler<Target> = async ({ title, contents, startAt, endAt }: Target) => {
     const createdAt = new Date();
     const id = newID();
-    if (!contents || !title) return;
-    const newTarget: Target = {
-      id,
-      title,
-      contents,
-      createdAt,
-      startAt,
-      endAt,
-    };
-
+    if (!contents || !title || !endAt || !startAt) return;
+    const newTarget: Target = { id, title, contents, createdAt, startAt, endAt };
     await setTodo((prev) => [...prev, newTarget]);
     reset();
   };
 
-  const handleStartPage = () => {
-    navigate('/main');
-  };
+  const handleStartPage = () => navigate('/board');
 
   return (
     <OLayout>
-      <div className="flex justify-center">
+      <div className="flex items-center justify-center gap-3 ">
         <div className="flex flex-col w-full gap-2 sm:w-1/2 lg:w-1/3 xl:w-144">
-          {todo.map((item, index) => {
-            return <TargetCard key={index} {...item} onDelete={(id) => console.log(item)} />;
-          })}
           <form className="flex flex-col gap-2" onSubmit={handleSubmit(onSubmit)}>
             <OTextField {...register('title')} placeholder="목표를 작성해 주세요." />
             <OTextarea {...register('contents')} placeholder="구체적으로 작성해 주세요." />
@@ -60,6 +47,15 @@ const MainPage = () => {
             ''
           )}
         </div>
+        {todo.length ? (
+          <div className="flex flex-col w-full gap-3 overflow-y-scroll sm:w-1/2 lg:w-1/3 xl:w-144 h-[600px] p-2 bg-slate-100">
+            {todo.map((item, index) => {
+              return <TargetCard key={index} {...item} onDelete={(id) => console.log(item)} />;
+            })}
+          </div>
+        ) : (
+          ''
+        )}
       </div>
     </OLayout>
   );
