aa1b5c9afc25a6ee1d126f087add5810df21f9dd
choi joo young <68840733+cwd3469@users.noreply.github.com>
2024-01-01 22:58:52 +0900

    OVB-1 feat : BoardPage 적용

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -1,5 +1,16 @@
+import useCurrentTarget from '@/controllers/useCurrentTarget';
+import TargetCard from '@components/main/TargetCard';
+
 const BoardPage = () => {
-  return <div>BoardPage</div>;
+  const { target, createTarget, deleteTarget } = useCurrentTarget();
+
+  return (
+    <div>
+      {target.map((item, index) => {
+        return <TargetCard key={index} {...item} onDelete={deleteTarget} />;
+      })}
+    </div>
+  );
 };
 
 export default BoardPage;
