4a5f0880740dddf0f6389706d255b4e0b9f2e363
choi joo young <68840733+cwd3469@users.noreply.github.com>
2024-01-01 22:24:28 +0900

    OVB-1 feat : 불필요한 코드 삭제

--- src/components/common/OButton.tsx
+++ src/components/common/OButton.tsx
@@ -2,24 +2,6 @@ import { ButtonHTMLAttributes, FC } from 'react';
 import { cva, VariantProps } from 'class-variance-authority';
 import { cn } from '@/utils/twMerge';
 
-const ColorVariants = cva(``, {
-  variants: {
-    color: {
-      primary: 'bg-primary-400 text-primary-400 border-primary-400',
-      secondary: 'bg-secondary-400 text-secondary-400 border-secondary-400',
-      error: 'bg-error-400 text-error-400 border-error-400',
-      warning: 'bg-warning-500 text-warning-500 border-warning-500',
-      info: 'bg-info-400 text-info-400 border-info-400',
-      success: 'bg-success-400 text-success-400 border-success-400',
-      'inherit-text': 'bg-gray-300 text-gray-300 border-gray-300',
-      'inherit-white': 'bg-gray-300 text-gray-300 border-gray-300',
-    },
-  },
-  defaultVariants: {
-    color: 'primary',
-  },
-});
-
 const ButtonVariants = cva(``, {
   variants: {
     size: {
@@ -47,17 +29,6 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement>, VariantPr
 //bg , text , border
 
 const OButton: FC<ButtonProps> = ({ variant, size, color, children, ...props }) => {
-  const mainColor: { [key: string]: string } = {
-    primary: 'primary-400',
-    secondary: 'secondary-400',
-    error: 'error-400',
-    warning: 'warning-500',
-    info: 'info-400',
-    success: 'success-400',
-    'inherit-text': 'gray-300',
-    'inherit-white': 'gray-300',
-  };
-
   return (
     <button className={cn(ButtonVariants({ variant, size }))} {...props}>
       {children}
