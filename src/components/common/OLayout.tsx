4761c2a8188e5b91c62e343bdcada727e0b616ca
choi joo young <68840733+cwd3469@users.noreply.github.com>
2024-01-02 23:36:26 +0900

    OVB-1 feat : BoardPage OLayout 적용

--- src/components/common/OLayout.tsx
+++ src/components/common/OLayout.tsx
@@ -12,7 +12,7 @@ const OLayout = ({ children }: Props) => {
   return (
     <div className="min-h-screen ">
       <div className="container absolute -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
-        <div className="p-3 bg-slate-200">{children}</div>
+        <div className="p-3 bg-slate-200 rounded-md ">{children}</div>
       </div>
     </div>
   );
--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -1,15 +1,21 @@
 import useCurrentTarget from '@/controllers/useCurrentTarget';
+import { OLayout } from '@components/common';
 import TargetCard from '@components/main/TargetCard';
 
 const BoardPage = () => {
   const { target, createTarget, deleteTarget } = useCurrentTarget();
 
   return (
-    <div>
+    <OLayout>
+      <div className="flex justify-between gap-2">
+        <div className="w-1/3 bg-slate-700 h-32" />
+        <div className="w-1/3 h-32 bg-slate-700" />
+        <div className="w-1/3 h-32 bg-slate-700" />
+      </div>
       {target.map((item, index) => {
         return <TargetCard key={index} {...item} onDelete={deleteTarget} />;
       })}
-    </div>
+    </OLayout>
   );
 };
 
