ff474225ddd26c4394abed30b9bfdfd9fe140edd
choi joo young <68840733+cwd3469@users.noreply.github.com>
2024-01-05 17:54:49 +0900

    OVB-1 feat :  eslint 변경 불필요한 extends 삭제

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,9 +1,20 @@
 module.exports = {
+  root: true,
   env: {
     browser: true,
-    es2021: true,
+    es2020: true,
   },
-  extends: ['standard-with-typescript', 'plugin:react/recommended', 'react-app', 'airbnb', 'prettier'],
+  extends: [
+    // 'airbnb',
+    // 'airbnb-typescript',
+    // 'airbnb/hooks',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+  ],
   overrides: [
     {
       env: {
@@ -15,10 +26,18 @@ module.exports = {
       },
     },
   ],
+  parser: '@typescript-eslint/parser',
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
+    project: ['./tsconfig.json'],
   },
-  plugins: ['react'],
-  rules: {},
+  plugins: ['@typescript-eslint', 'react', 'prettier'],
+  rules: {
+    'react/react-in-jsx-scope': 'off', // React 17 버전 이후 Import React from 'react' 없이도 코드 작성 가능
+    'no-use-before-define': 'off', // emotion css를 컴포넌트 파일 하단에서 관리하므로 off
+    '@typescript-eslint/no-use-before-define': 'off', // emotion css를 컴포넌트 파일 하단에서 관리하므로 off
+    'react/function-component-definition': [2, { namedComponents: ['arrow-function', 'function-declaration'] }], // arrow-function 허용
+  },
 };
