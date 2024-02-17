01ab4170aaf137acc48abbda3372be4903cd62e5
choi joo young <68840733+cwd3469@users.noreply.github.com>
2024-02-17 21:44:23 +0900

    OVB-1 feat : OLayout 정리

--- src/pages/BoardPage.tsx
+++ src/pages/BoardPage.tsx
@@ -1,16 +1,15 @@
 import useCurrentTarget from '@/stores/useCurrentTarget';
-import OLayout from '@/pages/layout/MainLayout';
 import TargetCard from '@components/main/TargetCard';
 
 const BoardPage = () => {
   const { target, createTarget, deleteTarget } = useCurrentTarget();
 
   return (
-    <OLayout>
+    <>
       {target.map((item, index) => {
         return <TargetCard key={index} {...item} onDelete={deleteTarget} />;
       })}
-    </OLayout>
+    </>
   );
 };
 
