33d433c5bd373eb22245ddb05f8d7ce61aa63de5
choi joo young <68840733+cwd3469@users.noreply.github.com>
2024-07-07 10:34:51 +0900

    OVB-1 feat : inputMessage 추가

--- src/utils/contextMsg.ts
+++ src/utils/contextMsg.ts
@@ -1,48 +1,13 @@
-type MsgType = {
-  state: 'error' | 'success';
-  context: string;
-};
+import { MessageBoxProps } from '@/components/common/textFiled';
+import { FieldErrors } from 'react-hook-form';
 
-type ContextMsgParam = { errType: string | undefined; keyValue: { [key: string]: string } };
-
-type StateMessageParam = {
-  keyValue: { [key: string]: string };
-};
-
-class StateMessage {
-  keyValue: { [key: string]: string };
-  constructor(params: StateMessageParam) {
-    this.keyValue = params.keyValue;
-  }
-  private contextMsg = (param: ContextMsgParam) => {
-    const { errType, keyValue } = param;
-    if (errType) {
-      const info: MsgType = {
-        state: 'error',
-        context: keyValue[errType],
-      };
-
-      return info;
-    }
-  };
-  createMsg = () => {
-    const msg = (errType?: string) => {
-      return this.contextMsg({ errType, keyValue: this.keyValue });
+export const inputMessage = (err: FieldErrors, name: string) => {
+  if (err[name] && err[name]?.message) {
+    const message: MessageBoxProps = {
+      state: 'error',
+      context: err[name]?.message as string,
     };
-    return msg;
-  };
-}
-
-const userIdMsg: { [key: string]: string } = {
-  required: '아이디를 입력해 주세요.',
-  pattern: '이메일 형식만 입력할 수 있습니다.',
+    return message;
+  }
+  return undefined;
 };
-const pwMsg: { [key: string]: string } = {
-  required: '비밀번호를 입력해 주세요.',
-  pattern: '영문자,숫자,특수문자 중 2개 이상을 사용하여 입력해 주세요',
-  minLength: '8-16자리를 입력해 주세요.',
-  maxLength: '8-16자리를 입력해 주세요.',
-};
-
-export const userIdMessage = new StateMessage({ keyValue: userIdMsg }).createMsg();
-export const pwMessage = new StateMessage({ keyValue: pwMsg }).createMsg();
